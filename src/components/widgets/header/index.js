8b00f48da29e71290907092e5f6693a27d341926
Fred Souza <fredericoamsouza@gmail.com>
2017-04-09 23:24:46 -0300

    Adding link to dashboard to header

--- src/components/widgets/header/index.js
+++ src/components/widgets/header/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
 import { Link } from 'react-router';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import { ProfileMenu } from './profileMenu';
 
 @inject('text')
@@ -19,6 +20,9 @@ export class Header extends React.Component {
                         </Navbar.Brand>
                     </Navbar.Header>
                     <Nav pullRight>
+                        <LinkContainer to={{ pathname: '/dashboard' }}>
+                            <NavItem>{text.get('views.dashboard.title')}</NavItem>
+                        </LinkContainer>
                         <ProfileMenu />
                     </Nav>
                 </Navbar>
